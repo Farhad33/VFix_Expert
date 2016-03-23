d358b06e5cd7b5c0d786f6b6f1c9172b82f7b042
dustynaugust <dustyn.august@gmail.com>
2016-03-22 20:50:50 -0700

    Added Twitter Login

--- VFix_Expert/VFix_Expert/LoginViewController.swift
+++ VFix_Expert/VFix_Expert/LoginViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import TwitterKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var twitterButtonView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let logInBUtton = TWTRLogInButton { (session, error) in
+            if let unwrappedSession = session {
+                let alert = UIAlertController(title: "Logged In",
+                    message: "User \(unwrappedSession.userName) has logged in",
+                    preferredStyle: UIAlertControllerStyle.Alert
+                )
+                alert.addAction(UIAlertAction(title: "OK",style: UIAlertActionStyle.Default, handler: nil))
+            } else {
+                NSLog("Login error: %@", error!.localizedDescription)
+            }
+        }
+        // TODO: Change where the log in button is positioned in your view
+        logInBUtton.center = self.twitterButtonView.center
+        self.view.addSubview(logInBUtton)
+        
         // Do any additional setup after loading the view.
     }
 
