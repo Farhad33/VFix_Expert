72cc4f6545aca6b0c12a7c5a168c445e83c55861
ndyoussfi <ndyoussfi@gmail.com>
2016-04-10 18:49:20 -0700

    Saving files

---
+++ VFix_Expert/VFix_Expert/defaultMaps.swift
@@ -0,0 +1,13 @@
+//
+//  defaultMaps.swift
+//  VFix_Expert
+//
+//  Created by Noureddine Youssfi on 4/7/16.
+//  Copyright © 2016 Dustyn August. All rights reserved.
+//
+
+import UIKit
+
+class defaultMaps: NSObject {
+
+}
---
+++ VFix_Expert/VFix_Expert/defaultMapsCell.swift
@@ -0,0 +1,24 @@
+//
+//  defaultMapsCell.swift
+//  VFix_Expert
+//
+//  Created by Noureddine Youssfi on 4/3/16.
+//  Copyright © 2016 Dustyn August. All rights reserved.
+//
+
+import UIKit
+
+class defaultMapsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
